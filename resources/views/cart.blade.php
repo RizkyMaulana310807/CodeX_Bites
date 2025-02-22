bdf9886086055490ab1d7677e81aa850aae18183
RizkyMaulana310807 <rizkymaulana317b@gmail.com>
2025-02-22 18:09:19 +0700

    Rombak ulang aplikasi

--- resources/views/cart.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout></x-layout>
-<h1 class="font-bold text-2xl px-6 mt-6">Your Ordered <strong class="text-[#d3737b]">Food!</strong></h1>
-<x-list-order></x-list-order>
-<x-list-order></x-list-order>
-<x-list-order></x-list-order>
-<x-list-order></x-list-order>
--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-<x-layout>
-
-</x-layout>
-<h1 class="font-bold text-2xl px-6 mt-6">Choose Your Favorite <strong class="text-[#d3737b]">Food!</strong></h1>
-
-<x-search></x-search>
-<x-tag></x-tag>
-<x-card></x-card>
